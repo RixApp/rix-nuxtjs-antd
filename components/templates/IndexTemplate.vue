46e8a793943c8356d694e55904ca0a0c38556afe
Giovanni D <giovanniyatze@gmail.com>
2020-01-17 01:12:40 -0600

    Add templates folder and index (main page template for testing usage)

---
+++ components/templates/IndexTemplate.vue
@@ -0,0 +1,28 @@
+<template class="template">
+  <div class="container">
+    Hi from IndexTemplate!
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component
+class IndexTemplate extends Vue {
+	
+  mounted () {
+  };
+}
+
+export default IndexTemplate;
+</script>
+
+<style style="sass" scoped>
+.container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #E6E4E4;
+}
+</style>
